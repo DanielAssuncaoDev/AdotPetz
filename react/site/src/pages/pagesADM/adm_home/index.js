cad8f0506dbcfb3710751fa5bb4404aa3afab5d5
WilliamSantos15 <williamsantos041015@gmail.com>
2021-09-24 11:42:40 -0300

    ok

---
+++ react/site/src/pages/pagesADM/adm_home/index.js
@@ -0,0 +1,18 @@
+// export default function(){
+//     return(
+//         <div className="box1">
+//             <div className="imgadm"></div>
+//             <div className="itens"> Nome do ADM: </div>
+//             <div className="itens"> E-mail: </div>
+//             <div className="itens"> Telefone (celular): </div>
+//             <div className="botao"> Sair </div>
+//         </div>
+//         <div className="box2">
+//             <div className= "itens-bx2"> Adicionar pet</div>
+//             <div className= "item-bbx2"> Ver pets cadastrados </div>
+//             <div className= "itens-bx2"> Ver solicitações de adoções</div>
+//         </div>
+//     )
+// }
+
+a
