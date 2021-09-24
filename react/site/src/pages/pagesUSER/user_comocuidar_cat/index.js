b223a325fd9114f0418e8b3c22a94f7ed193d0a7
WilliamSantos15 <williamsantos041015@gmail.com>
2021-09-24 11:21:18 -0300

    ok

---
+++ react/site/src/pages/pagesUSER/user_comocuidar_cat/index.js
@@ -0,0 +1,37 @@
+import Container from './styled.js' 
+
+import Cabecalho from '../../../components/cabecalhoUSU/index'
+
+export default function(){
+    return(
+        <Container>
+            <Cabecalho/>
+            <div className="Box-1">
+                <div className="Titulo">COMO CUIDAR DO MEU GATINHO</div>
+                <div className="imgcat">img</div>
+                <div className="recadinho">Adotou um gatinho e não sabe o que fazer para ser o dono perfeito? fique atento as dicas que daremos a seguir!!</div>    
+            </div>
+            <div className="Box-2"> 
+                <div className="titulo2">1- Cuidado com a alimentação</div>
+                <div className="desc">Lembre-se nunca dê ração demais para o seu gatinho. A obesidade é um dos maiores problemas dos felinos, então seja bem cuidadoso com a quantidade de ração que dará para o seu pet. Compre rações de boa qualidade e que sejam específicas para a idade do seu gato, isso é de extrema importância.
+Não deixe a ração do seu gato exposta, caso deixe ele perderá o aroma e a crocância. Então alimente seu pet em pequenas porçês várias vezes ao dia. E claro, não esqueça da água. Seria bom se ela fosse trocada duas vezes ao dia para que o pet beba sempre a água fresquinha.</div>
+                <div className="titulo2">2- Providencie uma caminha confortável</div>
+                <div className="desc">O lugar para o seu gatinho descansar é super importante também. Providencie uma cama confortável para ele, sem petshops existem diversas opções. Mas garanto que até mesmo uma caixa de papelão com um cobertorzinho dentro já seria o suficiente para deixá-lo aconchegado e satisfeito.</div>
+                <div className="titulo2">3- Banho</div>
+                <div className="desc">Normalmente os gatos fazem sua própria higiene, sendo assim eles geralmente não precisam de banhos com muita frequência. A não ser que por algum motivo o seu gato apresente problemas de pele ou de  pelos. Em casos desse tipo, o nosso conselho é que você leve-o ao veterinário para que ele possa ser tratado de forma correta e específica.
+OBS.: Para evitar que pelos caem por todo canto da casa, escove os pelos do seu gato com frequência, aliás alguns deles adoram ser escovado.
+ </div>
+                <div className="titulo2">4- Adestre seu gato</div>
+                <div className="desc">Animais não são nem um pouco fácies de serem adestrados. Mas sempre que seu petzinho realizar alguma ação que te incomode, não tente repreendê-lo usando violência!!!! Você pode evitar esses comportamentos indesejados utilizando um espirrando água com um borrifador. Mas de maneira alguma, NUNCA bata no seu gato, violência não o fará associar ao que ele acabou de fazer. Podendo até mesmo dificultar a relação entre vocês.</div>
+                <div className="titulo2">5- Janelas teladas</div>
+                <div className="desc">Se possível, instale telas em TODAS as janelas. Gatos são felinos muito ágeis e espertos, mas assim como nós eles também se distraem! Já ocorreram vários casos de gatos cairem da janela de apertamentos e se machucarem feio ou até mesmo morrerem. Então, sabendo disso faça o que puder para evitar que o mesmo ocorra com o seu gatinho e  instale telas em todas as janelas.</div>
+                <div className="titulo2">6- Saúde</div>
+                <div className="desc">Vacine seu gatinho anuelmente. Dê para o mesmo vermífugos e antipugas ao menos a cada 5 meses. Fique sempre atento ao apetite, urina, fezes e até mesmo no comportamento no seu pet. Caso note alguma alteração, leve-o ao veterinário para verificar se há algo de errado. Faça isso anualmente.</div>
+                
+            </div>
+        
+
+
+        <Container/>
+    )
+}
