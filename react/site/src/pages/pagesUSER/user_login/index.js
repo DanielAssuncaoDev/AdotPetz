305fd7ea7bf673e65d2be11c651eeeaa1e1d9b1d
Erik <erik.alexandre0912@gmail.com>
2021-09-26 23:55:06 -0300

    Eriker

---
+++ react/site/src/pages/pagesUSER/user_login/index.js
@@ -0,0 +1,64 @@
+import {Container, Conteudo} from './styled'
+
+export default function LoginUsu() {
+
+return (
+    <Container> 
+    
+
+<Conteudo>
+
+<div className="title">
+          Para conseguir ver mais informações do pet, por favor, cadastre-se no nosso site!
+        </div>
+
+        <div class="existente">
+          ENTRAR COM CONTA EXISTENTE
+        </div>
+
+        <div class="input">
+
+          <div class="inp">
+            <input type="text" />
+          </div>
+
+          <div class="pin">
+            <input type="text" />
+          </div>
+
+        </div>
+
+        <div class="buton">
+          <button>Entrar</button>
+        </div>
+
+        <div class="mudar">
+
+          <div class="adria">
+            <img src="Line 1.png" alt="" />
+          </div>
+
+          <div class="adulto">OU</div>
+
+          <div class="boll">
+            <img src="Line 1.png" alt="" />
+          </div>
+
+        </div>
+
+        <div class="logue">Logue com suas redes sociais</div>
+
+        <div class="face">
+          
+        </div>
+
+        <div class="cachorro">
+          <img src="image 1 (3).png" alt="" />
+        </div>
+
+</Conteudo>
+
+    </Container>
+);
+
+}
