58dcd1e2b5a7b0b258edd6b2031f1da1f10e6a52
skGomes <phgomes.sp2@gmail.com>
2021-10-04 21:32:08 -0300

    okokok

---
+++ react/site/src/pages/pagesUSER/user_FAQ/index.js
@@ -0,0 +1,26 @@
+import {Container} from'./styled'
+import CabecalhoUSO from'../../../components/comun/cabecalhoUSU';
+
+export default function FAQ(){
+
+    return(
+
+        <Container>
+            
+
+
+
+
+        </Container>
+
+
+
+
+
+
+
+
+
+
+    );
+}
