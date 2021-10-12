78d1c108304f292858b1047fc72fcc0f20791c97
skGomes <phgomes.sp2@gmail.com>
2021-10-12 20:46:41 -0300

    pedro

--- react/site/src/pages/pagesUSER/user_FAQ/index.js
+++ react/site/src/pages/pagesUSER/user_FAQ/index.js
@@ -1,23 +1,29 @@
 import { Container, Conteudo } from './styled';
 // import cabecalhoUSU from '../../../components/comun/cabecalhoUSU';
 
+import CabecalhoADM from '../../../components/comun/cabecalhoUSU';
+
+import rodape from '../../../components/comun/rodape';
+
 export default function FAQ(){
 
   return(
 
         <Container>
+                <CabecalhoADM />
+                
 
            
     <Conteudo> 
                                 
-                     <div className="titulo"> FAQ (Perguntas frequentes)</div>
+                    <h2> <div className="titulo"> FAQ (Perguntas frequentes)</div></h2>
 
-                        <div className="pergunta1">O site cobra alguma taxa de adoção?</div>
+                        <h2><div className="pergunta1">O site cobra alguma taxa de adoção?</div></h2>
 
                                 <div className="pergunta1">
               Não! Não cobramos nenhuma taxa de adoção. Só cobramos responsabilidade daquele quem vai adotar, apenas isso.</div>
 
-                <div className="pergunta2">Quero adotar um animal, como faço isso?</div>
+              <h2><div className="pergunta2">Quero adotar um animal, como faço isso?</div></h2>
 
                         <div className="pergunta2">
                         Que bom que você quer isso. É bem mais simples do que parece.
@@ -33,8 +39,8 @@ export default function FAQ(){
                         confirmação do pedido de adoção aguarde até que algum responável pelo
                         animal da ONG entre em contato com você.</div> 
 
-                        <div className="pergunta3">
-                        Até quanto tempo o pedido de adoção fica em solicitação?</div>
+                        <h2><div className="pergunta3">
+                        Até quanto tempo o pedido de adoção fica em solicitação?</div></h2>
 
                         <div className="pergunta3">
                             Geralmente, os pedidos demoram de 5 a 10 dias a serem verificados. Pois
@@ -44,12 +50,16 @@ export default function FAQ(){
                             que isso, você pode mandar um email para: doaacaodepets@outlook.com, ou
                             até mesmo ligar para (11) 5555-5555, informando seu nome, RG e CEP</div>
 
-                            <div className="pergunta4">
-                            Tenho cachorros ou gatos para vender. Posso divulga-los aqui? </div>
+                            <h2><div className="pergunta4">
+                            Tenho cachorros ou gatos para vender. Posso divulga-los aqui? </div></h2>
+
+                              <div className="pergunta4">
+                               NÃO! Amigo não se compra, por isso desenvolvemos esse site de adoção.
+                              </div> 
 
     </Conteudo>
 
-
+           <rodape />
         </Container>
 
     );
--- react/site/src/pages/pagesUSER/user_FAQ/styled.js
+++ react/site/src/pages/pagesUSER/user_FAQ/styled.js
@@ -1,9 +1,89 @@
 import styled from "styled-components";
 
+
+
+//import fundo from '../../../assets/images/fundoquemsomos.svg'
+
+import Imgfundo from "../../../assets/images/imagemfundo_home.svg"
+
+
+
 export const Container = styled.div`
+.titulo {
+    text-align: center;
+    padding: 10px;
+    background-image: url(${Imgfundo});
+    background-size: auto 100%;
+
+    background-repeat: no-repeat;
+  }
+  
+  .pergunta1 {
+    padding: 10px;
+    padding: 0px 25px 25px 25px;
+  display: flex;
+  flex-direction: column;
+  }
+  
+  .pergunta2 {
+    padding: 10px;
+    padding: 0px 25px 25px 25px;
+  display: flex;
+  flex-direction: column;
+  }
+  
+  .pergunta3 {
+    padding: 10px;
+    padding: 0px 25px 25px 25px;
+  display: flex;
+  flex-direction: column;
+  }
+  
+  .pergunta4 {
+    padding: 10px;
+    padding: 0px 25px 25px 25px;
+  display: flex;
+  flex-direction: column;
+  }
+
 
 `
 
 export const Conteudo = styled.div`
+
+.titulo {
+    text-align: center;
+    padding: 10px;
+  }
+  
+  .pergunta1 {
+    padding: 10px;
+    padding: 0px 25px 25px 25px;
+  display: flex;
+  flex-direction: column;
+  }
+  
+  .pergunta2 {
+    padding: 10px;
+    padding: 0px 25px 25px 25px;
+  display: flex;
+  flex-direction: column;
+  }
+  
+  .pergunta3 {
+    padding: 10px;
+    padding: 0px 25px 25px 25px;
+  display: flex;
+  flex-direction: column;
+  }
+  
+  .pergunta4 {
+    padding: 10px;
+    padding: 0px 25px 25px 25px;
+  display: flex;
+  flex-direction: column;
+  }
+
+  
 
 `
