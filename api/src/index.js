a367f3f671cd87375c063a53deb40af2c474fbfb
William <williamsantos041015@gmail.com>
2021-10-14 16:29:44 -0300

    aaaaaaaaaaaaaa

--- api/src/index.js
+++ api/src/index.js
@@ -1,15 +1,9 @@
 // import db from './db.js'
 import express from 'express'
 import cors from 'cors'
-
-    
-    const app = express()
-    app.use(cors())
-    app.use(express.json())
-
-// rbdjyyudybdcbutytrurbxcbtyrdxctudxtyedx5txtxtudxetudxdexxtexsteuxed rytufdryiddrdtru6d8u6d6 
-// tuf7ytrifruyfitryryiyirfddr9i
-// tyifd8y6rdtuyrdttrdtuedetruesdtduturydrutydr6utdurdruyt
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 
 
