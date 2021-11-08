08807f7888527f039fcdd9a9348f5d3cd218328d
erikal21 <erik.alexandre0912@gmail.com>
2021-11-08 11:51:52 -0300

    ok

--- react/site/src/pages/pagesADM/adm_animais_cadastrados/index.js
+++ react/site/src/pages/pagesADM/adm_animais_cadastrados/index.js
@@ -43,7 +43,7 @@ export default function AnimaisCadastrados(){
                             toast(r.erro)
                         }
                         else{
-                            toast.dark('Adoção cancelada') 
+                            toast.dark('Animal removido') 
                         }
         
                         listarAnimaisCadastrados()
