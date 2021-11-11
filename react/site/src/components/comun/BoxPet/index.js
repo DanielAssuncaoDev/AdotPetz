b55b0391da22bcd29a94152d0665baa25dae1b11
WilliamSantos15 <williamsantos041015@gmail.com>
2021-11-11 11:58:09 -0300

    ok

--- react/site/src/components/comun/BoxPet/index.js
+++ react/site/src/components/comun/BoxPet/index.js
@@ -3,77 +3,90 @@ import { BoxPet } from "../BoxPet/styled"
 import { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 
-export default function BoxPETZ(props){
+import {api} from '../../../service/api'
 
-const [colorPorte, setColorPorte] = useState('')
+export default function BoxPETZ(props) {
 
-const nav = useHistory()
+  const [colorPorte, setColorPorte] = useState('')
 
-useEffect( () => {
-    if( props.Animal.DS_SEXO === 'Macho' ){
+  const nav = useHistory()
+
+  useEffect(() => {
+    if (props.Animal.DS_SEXO === 'Macho') {
       setColorPorte('#101BC4')
     } else {
       setColorPorte('rgb(207, 40, 160)')
     }
+
+  }, [props])
+
+  function getImage() {
+    //return props.Animal.IMG_PET1;
   
-}, [props] )
-  
+    if (!props.Animal.IMG_PET1)
+      return '/assets/images/imagemPET.jpg';
+    else if (props.Animal.IMG_PET1.includes('http'))
+      return props.Animal.IMG_PET1;
+    else 
+      return api.defaults.baseURL + '/pets/imagem?url=' + props.Animal.IMG_PET1;
+  }
 
-    return(
-      <BoxPet colorPet={colorPorte}  >
-          {/* <div className='conteudo-pet'>    */}
-            <div className="imagemPET">
-                <img src={props.Animal.IMG_PET1} alt='' /> 
+
+  return (
+    <BoxPet colorPet={colorPorte}  >
+      {/* <div className='conteudo-pet'>    */}
+      <div className="imagemPET">
+        <img src={getImage()} alt='' />
+      </div>
+
+      <div className="NomePET"> <b>{props.Animal.NM_PET}</b> </div>
+      <div className="local"> São Paulo - SP </div>
+
+      <div className="sex">
+        <div className="info-sex"> {props.Animal.DS_SEXO} </div>
+        <div className="img-sex">
+          {
+            props.Animal.DS_SEXO === 'Macho'
+
+              ? <img src="/assets/images/sexo-macho.svg" alt="" />
+
+              : <img src="/assets/images/sexo-femea.svg" alt="" />
+
+          }
+        </div>
+      </div>
+
+      {
+        props.Animal.DS_PORTE === 'Pequeno'
+          ?
+          <div className="porte">
+            <div className="info-porteSelect"> P </div>
+            <div className="info-porte"> M </div>
+            <div className="info-porte"> G </div>
+          </div>
+
+          : props.Animal.DS_PORTE === 'Médio'
+
+            ?
+            <div className="porte">
+              <div className="info-porte"> P </div>
+              <div className="info-porteSelect"> M </div>
+              <div className="info-porte"> G </div>
             </div>
-            
-            <div className="NomePET"> <b>{props.Animal.NM_PET}</b> </div>
-            <div className="local"> São Paulo - SP </div>
-                
-            <div className="sex">
-                <div className="info-sex"> {props.Animal.DS_SEXO} </div>
-                <div className="img-sex">
-                  {
-                    props.Animal.DS_SEXO === 'Macho'
 
-                    ? <img src="/assets/images/sexo-macho.svg" alt="" /> 
-
-                    : <img src="/assets/images/sexo-femea.svg" alt="" /> 
-
-                  }
-                </div>
+            :
+            <div className="porte">
+              <div className="info-porte"> P </div>
+              <div className="info-porte"> M </div>
+              <div className="info-porteSelect"> G </div>
             </div>
-            
-              {
-                props.Animal.DS_PORTE === 'Pequeno'
-                  ? 
-                    <div className="porte">
-                      <div className="info-porteSelect"> P </div>
-                      <div className="info-porte"> M </div>
-                      <div className="info-porte"> G </div>
-                    </div>
-
-                  : props.Animal.DS_PORTE === 'Médio'
-
-                  ?
-                    <div className="porte">
-                      <div className="info-porte"> P </div>
-                      <div className="info-porteSelect"> M </div>
-                      <div className="info-porte"> G </div>
-                    </div>
-
-                  : 
-                    <div className="porte">
-                      <div className="info-porte"> P </div>
-                      <div className="info-porte"> M </div>
-                      <div className="info-porteSelect"> G </div>
-                    </div>
 
 
-              }
-                
-            <div className='botão'> <button onClick={ () => nav.push({pathname: '/pet', state: props.Animal }) } >Conhecer mais o Pet</button></div>
-          {/* </div>  */}
-      </BoxPet>
-        
-    )
+      }
+
+      <div className='botão'> <button onClick={() => nav.push({ pathname: '/pet', state: props.Animal })} >Conhecer mais o Pet</button></div>
+      {/* </div>  */}
+    </BoxPet>
+
+  )
 }
--- react/site/src/pages/pagesADM/adm_adicionar_pet/index.js
+++ react/site/src/pages/pagesADM/adm_adicionar_pet/index.js
@@ -9,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Api from '../../../service/api';
 const api = new Api()
 
-export default function Addpet(){
+export default function Addpet() {
     const nav = useHistory();
     const [nome, setNome] = useState('')
     const [especie, setEspecie] = useState('')
@@ -24,13 +24,13 @@ export default function Addpet(){
     const [imgPet3, setImgpet3] = useState('')
     const [castrado, setCastrado] = useState(false)
     const [vacinaV10, setVacinaV10] = useState(false)
-    const [vacinaV8, setVacinaV8] = useState({name:"vacinaV8", value: false })
+    const [vacinaV8, setVacinaV8] = useState({ name: "vacinaV8", value: false })
     const [vacinaV5, setVacinaV5] = useState(false)
     const [vacinaV4, setVacinaV4] = useState(false)
     const [vacinaV3, setVacinaV3] = useState(false)
     const [vacinaAntirrabica, setVacinaAntirrabica] = useState(false)
 
-// Oi lindo, comentei só pra fazer deploy rsrsrsrs. quando for mecher, pode descomentar rsrsrsrs "mecher" tá Serto
+    // Oi lindo, comentei só pra fazer deploy rsrsrsrs. quando for mecher, pode descomentar rsrsrsrs "mecher" tá Serto
 
     // async function adicionarPets() {
     //     let r = await Api.adicionarPets({ nome, especie, raca, sexo, peso, nascimento, porte, descricao, imgPet1, imgPet2, imgPet3, 
@@ -43,82 +43,83 @@ export default function Addpet(){
     //         }
     //  } 
 
-     async function inserirPet() {
+    async function inserirPet() {
         let formData = new FormData();
-            formData.append('nome', nome);
-            formData.append('especie', especie);
-            formData.append('raca', raca);
-            formData.append('sexo', sexo);
-            formData.append('peso', peso);
-            formData.append('nascimento', nascimento);
-            formData.append('porte', porte);
-            formData.append('descricao', descricao);
-            formData.append('imgPet1', imgPet1);
-            // formData.append('imgPet2', imgPet2);
-            // formData.append('imgPet3', imgPet3);
-            // formData.append('castrado', castrado);
-            // formData.append('vacinaV10', vacinaV10);
-            // formData.append('vacinaV8', vacinaV8);
-            // formData.append('vacinaV5', vacinaV5);
-            // formData.append('vacinaV4', vacinaV4);
-            // formData.append('vacinaV3', vacinaV3);
-            // formData.append('vacinaAntirrabica', vacinaAntirrabica);
+        formData.append('nome', nome);
+        formData.append('especie', especie);
+        formData.append('raca', raca);
+        formData.append('sexo', sexo);
+        formData.append('peso', peso);
+        formData.append('nascimento', nascimento);
+        formData.append('porte', porte);
+        formData.append('descricao', descricao);
+        formData.append('imgPet1', imgPet1);
+        // formData.append('imgPet2', imgPet2);
+        // formData.append('imgPet3', imgPet3);
+        // formData.append('castrado', castrado);
+        // formData.append('vacinaV10', vacinaV10);
+        // formData.append('vacinaV8', vacinaV8);
+        // formData.append('vacinaV5', vacinaV5);
+        // formData.append('vacinaV4', vacinaV4);
+        // formData.append('vacinaV3', vacinaV3);
+        // formData.append('vacinaAntirrabica', vacinaAntirrabica);
         //  if(especie === 'Canina' && vacinaV5 === true || vacinaV4 === true || vacinaV3 === true){
         //     return(toast.error("Você não pode inserir Vacinas de Gatos para Cães"))
         // } if(especie === 'Felina' && vacinaV8 === true || vacinaV10 === true) {
         //     return (toast.error("Você não pode inserir Vacinas de Cães para Gatos"))
         // if(nome && especie && raca && sexo && peso && nascimento && porte && descricao === ('')){
         //     return toast.error('Preencha os campos vazios')
-        
 
-    if(nome === ('')){
-        return toast.error('Nome inválido');
-    } if(especie === ('')){
-        return toast.error('Espécie inválida');
-    } if(raca === ('')){
-        return toast.error('Raça inválida');
-    } if(sexo === ('')){
-        return toast.error('Sexo inválido');
-    } if(peso === ('')){
-        return toast.error('Peso inválido');
-    } if(nascimento === ('')){
-        return toast.error('Data inválida');
-    } if(porte === ('')){
-        return toast.error('Porte inválido');
-    } if(descricao.length > 250){
-        return toast.error('Números de caracteres atingido')
-    // } if(imgPet1 === ('')){
-    //     return toast.error('❌ Imagem inválida');
-    // } if(imgPet2 === ('')){
-    //     return toast.error('❌ Imagem inválido');
-    // } if(imgPet3 === ('')){
-    //     return toast.error('❌ Imagem inválido');
-    } else {
-        let r = await api.adicionarPets(nome, especie, raca, sexo, peso, nascimento, porte, descricao, imgPet1, imgPet2, imgPet3, 
-            castrado, vacinaV10, vacinaV8, vacinaV5, vacinaV4, vacinaV3, vacinaAntirrabica);
 
-            if(r.erro !== undefined){
-                return toast.error('Os campos devem ser preenchidos')
+        if (nome === ('')) {
+            return toast.error('Nome inválido');
+        } if (especie === ('')) {
+            return toast.error('Espécie inválida');
+        } if (raca === ('')) {
+            return toast.error('Raça inválida');
+        } if (sexo === ('')) {
+            return toast.error('Sexo inválido');
+        } if (peso === ('')) {
+            return toast.error('Peso inválido');
+        } if (nascimento === ('')) {
+            return toast.error('Data inválida');
+        } if (porte === ('')) {
+            return toast.error('Porte inválido');
+        } if (descricao.length > 250) {
+            return toast.error('Números de caracteres atingido')
+            // } if(imgPet1 === ('')){
+            //     return toast.error('❌ Imagem inválida');
+            // } if(imgPet2 === ('')){
+            //     return toast.error('❌ Imagem inválido');
+            // } if(imgPet3 === ('')){
+            //     return toast.error('❌ Imagem inválido');
+        } else {
+            let r = await api.adicionarPets(formData);
+            // let r = await api.adicionarPets(nome, especie, raca, sexo, peso, nascimento, porte, descricao, imgPet1, imgPet2, imgPet3,
+            //     castrado, vacinaV10, vacinaV8, vacinaV5, vacinaV4, vacinaV3, vacinaAntirrabica);
+
+            if (r.erro !== undefined) {
+                return toast.error(r.erro)
             }
 
             toast('Pet cadastrado')
 
+        }
+
+
     }
 
-        
-     }
-    
     function previewImage() {
-         if (imgPet1) {
-         return URL.createObjectURL(imgPet1);
+        if (imgPet1) {
+            return URL.createObjectURL(imgPet1[0]);
         } else {
-            return"/assets/images/pet1.svg"
+            return "/assets/images/pet1.svg"
         }
     }
 
     function selectFile() {
         let input = document.getElementById("Imgs");
-            input.click()
+        input.click()
     }
 
     useEffect(() => {
@@ -130,63 +131,63 @@ export default function Addpet(){
         const IniciarChecked = () => {
             let vacinas = [vacinaV8]
 
-            for ( let v of vacinas){
+            for (let v of vacinas) {
 
-                if(v.value === true){
+                if (v.value === true) {
                     let checkbox = document.getElementById(v.name)
                     checkbox.setAttribute('checked', 'checked')
                 }
 
             }
-        } 
-        
+        }
+
         IniciarChecked()
     }, [])
 
-const alterarvacinas = (setVacina, vacina) => {
+    const alterarvacinas = (setVacina, vacina) => {
 
-        if( vacina.value === true ){
-            setVacina({name: vacina.name, value: false})
-        } else{
-            setVacina({name: vacina.name, value: true})
+        if (vacina.value === true) {
+            setVacina({ name: vacina.name, value: false })
+        } else {
+            setVacina({ name: vacina.name, value: true })
         }
     }
 
-    return(
+    return (
         <Container>
-        <ToastContainer/>
-            <Cabecalho/>
+            <ToastContainer />
+            <Cabecalho />
             <div className="container">
-            <div> <button className="vltr"> <img onClick={ () => nav.push('/admin/home') } className="vlt" src="/assets/images/icon_voltar.svg" alt=""/> </button> </div>
+                <div> <button className="vltr"> <img onClick={() => nav.push('/admin/home')} className="vlt" src="/assets/images/icon_voltar.svg" alt="" /> </button> </div>
                 <div className="box">
                     <div className="box-esq">
                         <div className="imgs">
-                            <img className="addimg" onClick={selectFile} src={previewImage()} alt=""/>
-                            <div className="input-file"> 
-                                <input className="bdimg" id="Imgs" type="file" onChange={e => setImgPet1(e.target.files[0])} />
-                            </div> 
+                            <img className="addimg" onClick={selectFile} src={previewImage()} alt="" />
+                            <div className="input-file">
+                                <input className="bdimg" id="Imgs" type="file" multiple={true} onChange={e => setImgPet1(e.target.files)} />
+                            </div>
                         </div>
                         <div className="Load-img"> <button className="Loadimg" onClick={selectFile}> Carregar Imagem </button></div>
                         <div className="inputs">
-                             <input className="input1" type="text" placeholder="Digite o Nome do Pet" value={nome} onChange={e => setNome(e.target.value)} />
+                            <input className="input1" type="text" placeholder="Digite o Nome do Pet" value={nome} onChange={e => setNome(e.target.value)} />
                         </div>
                         <div className="inputs">
                             <select className="select2" name="select" value={sexo} onChange={e => setSexo(e.target.value)}>
                                 <option value="SelecioneoSexo">Selecione o Sexo</option>
                                 <option value="Macho">Macho</option>
                                 <option value="Fêmea">Fêmea</option>
-                            </select> 
-                            <input className="input2" type="text" placeholder="Peso" value={peso} onChange={e => setPeso(e.target.value)}/> 
-                            <select className="select2" name="select" value={porte} onChange={e => setPorte(e.target.value)}> 
+                            </select>
+                            <input className="input2" type="text" placeholder="Peso" value={peso} onChange={e => setPeso(e.target.value)} />
+                            <select className="select2" name="select" value={porte} onChange={e => setPorte(e.target.value)}>
                                 <option value="SelecioneoPorte">Selecione o Porte</option>
                                 <option value="Grande">Grande</option>
                                 <option value="Médio">Médio</option>
-                                <option value="Pequeno">Pequeno</option> 
+                                <option value="Pequeno">Pequeno</option>
                             </select>
                         </div>
                         <div className="inputs">
-                            <input className="input3" type="text" placeholder="Nascimento" value={nascimento} onChange={e => setNascimento(e.target.value)}/> 
-                            <select className="select3" name="select" value={especie} onChange={e => setEspecie(e.target.value)}> 
+                            <input className="input3" type="text" placeholder="Nascimento" value={nascimento} onChange={e => setNascimento(e.target.value)} />
+                            <select className="select3" name="select" value={especie} onChange={e => setEspecie(e.target.value)}>
                                 <option value="">Espécie</option>
                                 <option value="Canina">Canina</option>
                                 <option value="Felina">Felina</option>
@@ -215,7 +216,7 @@ const alterarvacinas = (setVacina, vacina) => {
                             <div className="checkvcn">
                                 <label className="label1" /*value={vacinaV10} onChange={e => setVacinaV10(e.target.value)}*/>  V10 </label>
                                 <input className="input5" type="checkbox"
-                                    
+
                                 />
                             </div>
                         </div>
@@ -223,15 +224,15 @@ const alterarvacinas = (setVacina, vacina) => {
                             <div className="tipopet"> VACINAS FELINAS </div>
                             <div className="checkvcn">
                                 <label className="label1"> V3 </label>
-                                <input className="input5" type="checkbox" /*value={vacinaV3} onChange={e => setVacinaV3(e.target.value)}*//>
+                                <input className="input5" type="checkbox" /*value={vacinaV3} onChange={e => setVacinaV3(e.target.value)}*/ />
                             </div>
                             <div className="checkvcn">
                                 <label className="label1"> V4 </label>
-                                <input className="input5" type="checkbox" /*value={vacinaV4} onChange={e => setVacinaV4(e.target.value)}*//>
+                                <input className="input5" type="checkbox" /*value={vacinaV4} onChange={e => setVacinaV4(e.target.value)}*/ />
                             </div>
                             <div className="checkvcn">
                                 <label className="label1"> V5 </label>
-                                <input className="input5" type="checkbox" /*value={vacinaV5} onChange={e => setVacinaV5(e.target.value)} *//>
+                                <input className="input5" type="checkbox" /*value={vacinaV5} onChange={e => setVacinaV5(e.target.value)} */ />
                             </div>
                         </div>
                         <div className="check">
@@ -247,7 +248,7 @@ const alterarvacinas = (setVacina, vacina) => {
                     </div>
                 </div>
                 <div className="botao">
-                    <button className="button1" onClick={ () => inserirPet()} > <img src="/assets/images/vector.svg" alt=""/> Cadastrar Pet </button>
+                    <button className="button1" onClick={() => inserirPet()} > <img src="/assets/images/vector.svg" alt="" /> Cadastrar Pet </button>
                 </div>
             </div>
         </Container>
